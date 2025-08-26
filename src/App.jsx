a669f6cd862452c062893d67c4c4479bb05b8502
Evann63 <evanderjuventino@gmail.com>
2025-08-26 13:50:23 +0700

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,3 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import navbar from "/navbar";
-import home from "/home";
-import about from "/about";
-import program from "/program";
-
 export default function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Program" element={<Program />} />
-      </Routes>
-    </Router>
-  );
+  return <h1 style={{ color: 'red' }}>Hello World</h1>;
 }
